3e5a2c3f735ef90be759945735967522b8b11c3d
bakpakin <calsrose@gmail.com>
2018-03-20 19:01:37 -0400

    .

---
+++ iOS/FooFii/GeofireExperimentController.swift
@@ -0,0 +1,73 @@
+//
+//  GeofireExperimentController.swift
+//  FooFii
+//
+//  Created by Calvin Rose on 3/20/18.
+//  Copyright © 2018 Calvin Rose. All rights reserved.
+//
+
+
+
+
+
+
+// This is just an experiment
+
+
+
+
+
+
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class GeofireExperimentController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue = manager.location else { return }
+        centerMapOnLocation(location: locValue)
+        //locationManager.stopUpdatingLocation()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    let regionRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius, regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
